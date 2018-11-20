85e3bf9e2ab94afc00f40ae35f273a7402c6f5cd
Kenji Tadashi Tsutsumi Rebolledo <DonPolluelo@localhost.localdomain>
2018-11-19 22:40:08 -0300

    creando el repo

---
+++ app/src/main/java/com/pichangaexample/pichanga/MainActivity.java
@@ -0,0 +1,42 @@
+package com.pichangaexample.pichanga;
+
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+    public class MainActivity extends AppCompatActivity {
+
+        private TabLayout tabLayout;
+        private ViewPager viewpager;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            //Toolbar
+            /*Toolbar myToolbar = (Toolbar) findViewById(R.id.app_bar);
+            setSupportActionBar(myToolbar);
+
+            String horario = getResources().getString(R.string.horario);
+            String malla = getResources().getString(R.string.malla);
+            String semestre = getResources().getString(R.string.semestre);*/
+
+            String mis_ligas = "Mis Ligas";
+            String ligas_seguidas = "Ligas Seguidas";
+
+
+            tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+            viewpager = (ViewPager) findViewById(R.id.viewpager_id);
+            ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+            // adapter.AddFragment(new HorarioFragment(), horario);
+            adapter.AddFragment(new MisLigasFragment(), mis_ligas);
+            adapter.AddFragment(new LigasSeguidasFragment(), ligas_seguidas);
+
+            viewpager.setAdapter(adapter);
+            tabLayout.setupWithViewPager(viewpager);
+        }
+    }
---
+++ app/src/main/java/com/pichangaexample/pichanga/MisLigasFragment.java
@@ -0,0 +1,28 @@
+package com.pichangaexample.pichanga;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MisLigasFragment extends Fragment {
+
+    public MisLigasFragment() {
+    }
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.mis_ligas_fragment, container, false);
+        return view;
+    }
+
+
+
+
+}
