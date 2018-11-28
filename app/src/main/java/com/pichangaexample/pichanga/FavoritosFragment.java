dbf1052bb767c94b0da96cdf9df14b61b933ea1b
Raurur00 <javierzavalatorres@gmail.com>
2018-11-28 14:13:27 -0300

    Floating action button

--- app/src/main/java/com/pichangaexample/pichanga/FavoritosFragment.java
+++ app/src/main/java/com/pichangaexample/pichanga/FavoritosFragment.java
@@ -1,7 +1,10 @@
 package com.pichangaexample.pichanga;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,15 @@ public class FavoritosFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favoritos, container, false);
+        View view = inflater.inflate(R.layout.fragment_favoritos, container, false);
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.floatingButtonCrearLiga);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),CrearLigaActivity.class);
+                startActivity(i);
+           }
+        });
+        return view;
     }
 }
