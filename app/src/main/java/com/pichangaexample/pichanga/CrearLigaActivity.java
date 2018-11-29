8fdcf40cb0c7d48373ee18ee2627cf4fdc4102d1
Raurur00 <javierzavalatorres@gmail.com>
2018-11-29 02:50:48 -0300

    Agregar Liga

--- app/src/main/java/com/pichangaexample/pichanga/CrearLigaActivity.java
+++ app/src/main/java/com/pichangaexample/pichanga/CrearLigaActivity.java
@@ -1,5 +1,6 @@
 package com.pichangaexample.pichanga;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,17 +8,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.pichangaexample.pichanga.Adapters.EquipoAdapter;
 import com.pichangaexample.pichanga.Models.Equipo;
+import com.pichangaexample.pichanga.Models.Liga;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class CrearLigaActivity extends AppCompatActivity {
 
     private ArrayList<Equipo> listaEquipo;
     private EquipoAdapter adapter;
     private EditText txtInput;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +38,8 @@ public class CrearLigaActivity extends AppCompatActivity {
         ListView listview = (ListView) findViewById(R.id.listViewCrearLiga);
         adapter = new EquipoAdapter(this, listaEquipo);
         listview.setAdapter(adapter);
+
+        inicializarFirebase();
 
         txtInput = (EditText) findViewById(R.id.NombreEquipo);
         Button btAdd= (Button) findViewById(R.id.buttonAgregarEquipo);
@@ -44,5 +55,32 @@ public class CrearLigaActivity extends AppCompatActivity {
                 }
             }
         });
+
+        Button btCrear = (Button) findViewById(R.id.buttonCrearLiga);
+        btCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences prefs = getSharedPreferences("login", MODE_PRIVATE);
+                final String correo = prefs.getString("correo", "");
+                EditText nombre = (EditText) findViewById(R.id.NombreLiga);
+                String n = nombre.getText().toString();
+                if (!n.equals("")){
+                    Liga liga = new Liga();
+                    liga.setNombre(n);
+                    liga.setIdCreador(correo);
+                    liga.setID(UUID.randomUUID().toString());
+                    liga.setListaEquipos(listaEquipo);
+                    databaseReference.child("Liga").child(liga.getID()).setValue(liga);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Ingrese Nombre",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
 }
--- app/src/main/java/com/pichangaexample/pichanga/Models/Liga.java
+++ app/src/main/java/com/pichangaexample/pichanga/Models/Liga.java
@@ -3,7 +3,54 @@ package com.pichangaexample.pichanga.Models;
 import java.util.ArrayList;
 
 public class Liga {
+    private String ID;
+    private String idCreador;
     private String nombre;
     private ArrayList<Equipo> listaEquipos;
     private ArrayList<Partido> listaPartidos;
+
+    public Liga(){
+        listaEquipos = new ArrayList<>();
+        listaPartidos = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Equipo> getListaEquipos() {
+        return listaEquipos;
+    }
+
+    public void setIdCreador(String idCreador) {
+        this.idCreador = idCreador;
+    }
+
+    public ArrayList<Partido> getListaPartidos() {
+        return listaPartidos;
+    }
+
+    public String getIdCreador() {
+        return idCreador;
+    }
+
+    public void setListaEquipos(ArrayList<Equipo> listaEquipos) {
+        this.listaEquipos = listaEquipos;
+    }
+
+    public void setListaPartidos(ArrayList<Partido> listaPartidos) {
+        this.listaPartidos = listaPartidos;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
 }
