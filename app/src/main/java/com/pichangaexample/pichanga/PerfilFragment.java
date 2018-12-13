63e85c51f05b871102ea0ee760ea91c2f7fc01f7
Kenji Tadashi Tsutsumi Rebolledo <DonPolluelo@localhost.localdomain>
2018-12-13 02:21:51 -0300

    Se muestra la cantidad de ligas creadas en el perfil

--- app/src/main/java/com/pichangaexample/pichanga/PerfilFragment.java
+++ app/src/main/java/com/pichangaexample/pichanga/PerfilFragment.java
@@ -7,13 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class PerfilFragment extends Fragment {
     Integer i;
     String str="";
+
+    int listSize;
+    TextView t;
+
     View view;
 
     @Nullable
@@ -22,7 +37,44 @@ public class PerfilFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
         SharedPreferences prefs = getActivity().getSharedPreferences("login", MODE_PRIVATE);
-        String correo = prefs.getString("correo", "");
+        final String correo = prefs.getString("correo", "");
+
+
+
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference scoreRef = rootRef.child("Liga");
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> list = new ArrayList<>();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String nombre = ds.child("nombre").getValue(String.class);
+                    String creador = ds.child("idCreador").getValue(String.class);
+                    if(correo.equals(creador)) {
+                        list.add(nombre);
+                    }
+
+
+                }
+
+                listSize = list.size();
+                t=(TextView) getActivity().findViewById(R.id.textView3);
+                t.setText("Ligas Creadas por el Usuario: " + String.valueOf(listSize));
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        scoreRef.addListenerForSingleValueEvent(eventListener);
+
+
+
+
+
+
 
         for(i=0;i<correo.length();i++){
             if (correo.charAt(i)!='@'){
