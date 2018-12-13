b9ded18ce9e6616c59f9a3b445d61d79be507c57
Kenji Tadashi Tsutsumi Rebolledo <DonPolluelo@localhost.localdomain>
2018-12-13 01:37:29 -0300

    Se muestran las ligas creadas por el usuario en el fragmento MIS LIGAS. Además se muestra un listado con todas las ligas existentes (por ahora en LIGAS SEGUIDAS)

--- app/src/main/java/com/pichangaexample/pichanga/FavoritosFragment.java
+++ app/src/main/java/com/pichangaexample/pichanga/FavoritosFragment.java
@@ -9,6 +9,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FavoritosFragment extends Fragment {
 
@@ -16,6 +27,36 @@ public class FavoritosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_favoritos, container, false);
+
+
+
+
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference scoreRef = rootRef.child("Liga");
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> list = new ArrayList<>();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String nombre = ds.child("nombre").getValue(String.class);
+                    list.add(nombre);
+
+                }
+                ListView listView = (ListView) getActivity().findViewById(R.id.ligas_listview);
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        scoreRef.addListenerForSingleValueEvent(eventListener);
+
+
+
+
+
         return view;
     }
 }
--- app/src/main/java/com/pichangaexample/pichanga/LigasFragment.java
+++ app/src/main/java/com/pichangaexample/pichanga/LigasFragment.java
@@ -10,12 +10,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.github.clans.fab.FloatingActionMenu;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -26,6 +34,47 @@ public class LigasFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ligas, container, false);
+
+
+
+        SharedPreferences prefs = getActivity().getSharedPreferences("login", MODE_PRIVATE);
+        final String correo = prefs.getString("correo", "");
+
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference scoreRef = rootRef.child("Liga");
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> list = new ArrayList<>();
+                for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String nombre = ds.child("nombre").getValue(String.class);
+                    String creador = ds.child("idCreador").getValue(String.class);
+                    if(correo.equals(creador)) {
+                        list.add(nombre);
+                    }
+
+
+                }
+                ListView listView = (ListView) getActivity().findViewById(R.id.mis_ligas_listview);
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, list);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        scoreRef.addListenerForSingleValueEvent(eventListener);
+
+
+
+
+
+
+
+
+
+
         FloatingActionMenu actionMenu = (FloatingActionMenu) view.findViewById(R.id.menu_flotante);
 
         com.github.clans.fab.FloatingActionButton button1 = (com.github.clans.fab.FloatingActionButton)
@@ -59,8 +108,17 @@ public class LigasFragment extends Fragment {
             }
         });*/
 
-        SharedPreferences prefs = getActivity().getSharedPreferences("login", MODE_PRIVATE);
-        final String correo = prefs.getString("correo", "");
+
+
+
+
+
+
+
+
+
+        //SharedPreferences prefs = getActivity().getSharedPreferences("login", MODE_PRIVATE);
+        //final String correo = prefs.getString("correo", "");
         Query query = FirebaseDatabase.getInstance().getReference("Liga")
                 .orderByChild("idCreador")
                 .equalTo(correo);
